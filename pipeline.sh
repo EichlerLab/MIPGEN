b7dae09285a789f2b356502bad56c012eaaeabce
Nik Krumm <nkrumm@gmail.com>
2015-04-27 16:39:12 -0700

    Add timing/logging to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo `date` ": Starting..."
 
 # MODULES
 module load bwa/0.7.3 samtools/0.1.19 python/2.7.3 numpy/1.7.0 scipy/0.12.0b1 pear/0.9.5
@@ -27,6 +28,9 @@ TMP=$(mktemp -d)
 # COPY SOURCE to $TMP
 rsync -arv --bwlimit=15000 $BWA_INDEX_SOURCE* $TMP
 
+
+echo `date` ": Copying read FASTQ data to local temp directory $TMP)"
+
 rsync $READ_1 $TMP
 rsync $READ_2 $TMP
 rsync $READ_3 $TMP
@@ -36,27 +40,32 @@ READ_2_TMP=$TMP/`basename $READ_2`
 READ_3_TMP=$TMP/`basename $READ_3`
 
 # SCRIPT
+echo `date` ": Running mipgen_fq_cutter_pe.py..."
 python $MIPGEN/tools/mipgen_fq_cutter_pe.py \
     $READ_1_TMP $READ_3_TMP -i $READ_2_TMP \
     -j $BARCODE_LENGTH \
     -tb $BARCODES \
     -o $TMP/$OUTPREFIX.barcoded
 
+echo `date` ": Running pear..."
 pear -f $TMP/$OUTPREFIX.barcoded.r1.indexed.fq \
     -r $TMP/$OUTPREFIX.barcoded.r2.indexed.fq \
     -o $TMP/$OUTPREFIX.barcoded
 
+echo `date` ": Running mipgen_fq_cutter_se.py..."
 python $MIPGEN/tools/mipgen_fq_cutter_se.py \
     $TMP/$OUTPREFIX.barcoded.assembled.fastq \
     -tb $BARCODES \
     -m $MOLECULAR_TAG_SIZES \
     -o $TMP/$OUTPREFIX.barcoded
 
+echo `date` ": Running bwa and sorting..."
 bwa mem -t $THREADS $TMP/`basename $BWA_INDEX_SOURCE` \
     $TMP/$OUTPREFIX.barcoded.indexed.fq \
     | samtools view -bS - \
     | samtools sort - $TMP/$OUTPREFIX.barcoded.indexed.sort
 
+echo `date` ": Running mipgen_smmip_collapser.py..."
 samtools view -h $TMP/$OUTPREFIX.barcoded.indexed.sort.bam \
     | python $MIPGEN/tools/mipgen_smmip_collapser.py 5 \
         $TMP/$OUTPREFIX.barcoded.indexed.sort.collapse \
@@ -65,6 +74,7 @@ samtools view -h $TMP/$OUTPREFIX.barcoded.indexed.sort.bam \
         $BARCODES
 
 # COPY OUT FINAL
+echo `date` ": Saving results to $FINAL_DEST_DIR"
 rsync -arv $TMP/$OUTPREFIX.barcoded.indexed.sort.bam $FINAL_DEST_DIR
 
-echo "analysis commands have terminated"
+echo `date` ": Done."
